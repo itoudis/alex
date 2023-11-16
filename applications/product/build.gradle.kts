3adb3e97176da658a36fe30ca1ebbf35101eed70
Hüseyin Berk <huseyinberk.cs@gmail.com>
2023-11-16 20:25:45 +0300

    proje bağımlılıklarını yönetmek için buildSrc oluşturuldu, product app için module oluşturuldu

---
+++ applications/product/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+    id("multimodule-config")
+    id("spring-conventions")
+
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,29 +1,11 @@
-val javaVersion by extra(17)
-
 plugins {
-    id("java")
+    id("groovy-gradle-plugin")
 }
 
 repositories {
-    // TODO gereksiz varsa kaldır
-    gradlePluginPortal()
     mavenCentral()
-    maven { url = uri("https://plugins.gradle.org/m2/") }
 }
-
-java {
-    toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion)) }
-    withSourcesJar()
-}
-
-java.sourceCompatibility = JavaVersion.toVersion(javaVersion)
-
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.1.5")
 }
