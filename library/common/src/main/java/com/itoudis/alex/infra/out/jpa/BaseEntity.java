92df100b688e8fae9a7c097f4ac570a1dd4d3821
Hüseyin Berk <huseyinberk.cs@gmail.com>
2023-11-16 21:06:51 +0300

    Base entity oluşturuldu

---
+++ library/common/src/main/java/com/itoudis/alex/infra/out/jpa/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.itoudis.alex.infra.out.jpa;
+
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @Version
+    protected int version = 0;
+
+    @CreatedDate
+    protected LocalDateTime createdDate = LocalDateTime.now();
+
+    @LastModifiedDate
+    protected LocalDateTime lastModifiedDate = LocalDateTime.now();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntity that)) return false;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
