1ac78816751cc2688b73fb9c027bef13050a037c
emre.basar <emre.basar@tubitak.gov.tr>
2023-11-30 22:38:18 +0300

    security servisi eklendi

---
+++ applications/security/src/main/java/com/itoudis/alex/SecurityApplication.java
@@ -0,0 +1,11 @@
+package com.itoudis.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "alex"
 include("applications:product")
 include("library:common")
+include("applications:security")
