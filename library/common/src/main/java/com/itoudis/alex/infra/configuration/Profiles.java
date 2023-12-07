1348a2b8d5c847f20610d7beeda53c9cd6fa3bd9
EmreBasar98 <emre.basar.98@gmail.com>
2023-12-07 22:48:35 +0300

    Add profiles and dependency for common package

---
+++ library/common/src/main/java/com/itoudis/alex/infra/configuration/Profiles.java
@@ -0,0 +1,8 @@
+package com.itoudis.alex.infra.configuration;
+
+public final class Profiles {
+    public static final String DEVELOPMENT = "development";
+    public static final String TEST = "test";
+    public static final String PRODUCTION = "productoion";
+
+}
