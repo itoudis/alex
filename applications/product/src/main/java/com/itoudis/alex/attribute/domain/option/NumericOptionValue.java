95819189b003cec46506b7bc577dc6612c301b80
EmreBasar98 <emre.basar.98@gmail.com>
2024-01-15 18:58:59 +0300

    Deleting OptionValue impls since it has changed to a concrete class

--- applications/product/src/main/java/com/itoudis/alex/attribute/domain/option/NumericOptionValue.java
+++
@@ -1,9 +0,0 @@
-package com.itoudis.alex.attribute.domain.option;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class NumericOptionValue extends OptionValue {
-
-    private final int value;
-}
