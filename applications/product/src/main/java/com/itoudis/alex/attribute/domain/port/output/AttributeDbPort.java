05f6e34fe5b8623fee8c840e86e6503c26241c5e
Hüseyin Berk <huseyinberk.cs@gmail.com>
2024-01-18 23:10:01 +0300

    alex-2 created db port

---
+++ applications/product/src/main/java/com/itoudis/alex/attribute/domain/port/output/AttributeDbPort.java
@@ -0,0 +1,9 @@
+package com.itoudis.alex.attribute.domain.port.output;
+
+import com.itoudis.alex.attribute.domain.Attribute;
+
+public interface AttributeDbPort {
+
+    Attribute createAttribute(Attribute attribute);
+
+}
