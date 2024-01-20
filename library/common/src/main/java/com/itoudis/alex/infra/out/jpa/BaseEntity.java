bee121fe0da16b047b2346bb02d6ba7f5c8134e8
Hüseyin Berk <huseyinberk.cs@gmail.com>
2024-01-20 16:58:33 +0300

    alex-2 changed version of spring data jpa

--- library/common/src/main/java/com/itoudis/alex/infra/out/jpa/BaseEntity.java
+++ library/common/src/main/java/com/itoudis/alex/infra/out/jpa/BaseEntity.java
@@ -16,7 +16,7 @@ public abstract class BaseEntity implements Serializable {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 
